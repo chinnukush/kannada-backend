d09796056471b04810c6878638774d15b228ce10
chinnukush <chinnukushal123@gmail.com>
2025-11-01 13:21:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = "15671595"
 API_HASH = "bb8f36f9c39a24c7f8b2acbc7ea8c60a"
 BOT_TOKEN = "7313932976:AAFnh1bszQItH6U3bZAwCXwXj58m2AxyhcM"
 AUTH_CHANNEL = "-1003224239956"
-DATABASE = "mongodb+srv://anikush8310_db_user:A5qknHVIUlBegUqU@cluster0.9fb2nmj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE = "mongodb+srv://kyt43843_db_user:SCORy9uCMUR6wsC8@cluster0.msjutt0.mongodb.net/?appName=Cluster0"
 TMDB_API = "6abcb6bb99fb77f33c37016a28866ed2"
 IMDB_API = "https://imdb-api.pandapelisv.workers.dev"
 
