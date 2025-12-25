8ac4ed06f315be46c5a3ad2a676efa4f70bf0504
chinnukush <chinnukushal123@gmail.com>
2025-12-25 19:22:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,14 @@
-
-
-
-# Startup Config
 API_ID = "15671595"
 API_HASH = "bb8f36f9c39a24c7f8b2acbc7ea8c60a"
 BOT_TOKEN = "7313932976:AAFnh1bszQItH6U3bZAwCXwXj58m2AxyhcM"
-AUTH_CHANNEL = "-1003224239956"
-DATABASE = "mongodb+srv://kyt43843_db_user:SCORy9uCMUR6wsC8@cluster0.msjutt0.mongodb.net/?appName=Cluster0"
+AUTH_CHANNEL = "-1002515386092"
+DATABASE = "mongodb+srv://anikush8310_db_user:9PMnoz9pA37pQnDX@cluster0.oamg97c.mongodb.net/?appName=Cluster0"
 TMDB_API = "6abcb6bb99fb77f33c37016a28866ed2"
 IMDB_API = "https://imdb-api.pandapelisv.workers.dev"
-
-# SERVER
 BASE_URL = "unconscious-frieda-koyebuser123-c7a52ee0.koyeb.app"  
 PORT = "8000"
-
-# Update
 UPSTREAM_REPO = "https://github.com/chinnukush/kannada-backend"
 UPSTREAM_BRANCH = "kannada"
-
-# Additional CDN Bots
-# MULTI_TOKEN1 = "7390174586:AAGlfMP3iN70hjpHAfB8YcUna5tDgpLXC4w"
-
 USE_CAPTION = "True"
 USE_TMDB = "True"
 OWNER_ID = "7253187871"
