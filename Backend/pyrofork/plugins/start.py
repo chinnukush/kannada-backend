6f1032aaa24ff692fd825f43fa5484566729d614
chinnukush <chinnukushal123@gmail.com>
2025-12-30 11:37:39 +0530

    Update start.py

--- Backend/pyrofork/plugins/start.py
+++ Backend/pyrofork/plugins/start.py
@@ -357,6 +357,45 @@ async def schedule_post(bot, tmdb_id):
     movie_updates.pop(tmdb_id, None)
     pending_posts.pop(tmdb_id, None)
 
+@Client.on_message(filters.channel & filters.chat(Telegram.AUTH_CHANNEL))
+async def file_receive_handler(bot: Client, message: Message):
+    try:
+        file = message.video or message.document
+        title = message.caption if (Telegram.USE_CAPTION and message.caption) else file.file_name or file.file_id
+        size = file.file_size
+
+        metadata_info = await metadata(clean_filename(title), file)
+        if metadata_info is None:
+            return
+
+        tmdb_id = metadata_info.get("tmdb_id")
+        media_type = metadata_info.get("media_type", "movie")
+        poster = metadata_info.get("poster", None)
+
+        # Save to backend first
+        success = await save_to_backend(metadata_info, file, size, title)
+        if not success:
+            print("⚠️ Skipped update post because backend save failed")
+            return
+
+        movie_updates[tmdb_id] = {
+            "title": metadata_info.get("title", title),
+            "media_type": media_type,
+            "poster": poster,
+            "season_number": metadata_info.get("season_number"),
+            "episode_number": metadata_info.get("episode_number"),
+            "year": metadata_info.get("year")
+        }
+
+        if tmdb_id not in pending_posts:
+            pending_posts[tmdb_id] = asyncio.create_task(schedule_post(bot, tmdb_id))
+
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        await message.reply_text(f"Got Floodwait of {e.value}s")
+
+
+
 @Client.on_message(filters.command('caption') & filters.private & CustomFilters.owner)
 async def toggle_caption(bot: Client, message: Message):
     try:
